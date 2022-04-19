15d03ea183c1174212d9ae1f2ca13b831d46ef7f
nx-amiribenabdellah <amir.iben-abdellah@nexum.com>
2022-04-19 10:20:35 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,10 @@
-sudo apt-get update
-sudo apt-get install build-essential chrpath libssl-dev libxft-dev
-sudo apt-get install libfreetype6 libfreetype6-dev
-sudo apt-get install libfontconfig1 libfontconfig1-dev
+sudo apt update && sudo apt upgrade
+sudo apt install build-essential chrpath libssl-dev libxft-dev
+sudo apt install libfreetype6 libfreetype6-dev libfontconfig1 libfontconfig1-dev
 
-cd ~
-export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
-wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
-sudo tar xvjf $PHANTOM_JS.tar.bz2
-
-sudo mv $PHANTOM_JS /usr/local/share
-sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
-
+export PHANTOMJS="phantomjs-1.9.8-linux-x86_64"
+wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOMJS.tar.bz2
+sudo tar xvjf $PHANTOMJS.tar.bz2
+sudo mv $PHANTOMJS /usr/local/share
+sudo ln -sf /usr/local/share/$PHANTOMJS/bin/phantomjs /usr/local/bin
 phantomjs --version
-
-phantomjs ./src/app.js
