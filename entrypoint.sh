ee63060621435b1b44e0286bed492e023e709b69
nx-amiribenabdellah <amir.iben-abdellah@nexum.com>
2022-04-19 09:55:54 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ export PHANTOM_JS="phantomjs-1.9.8-linux-x86_64"
 wget https://bitbucket.org/ariya/phantomjs/downloads/$PHANTOM_JS.tar.bz2
 sudo tar xvjf $PHANTOM_JS.tar.bz2
 
-udo mv $PHANTOM_JS /usr/local/share
+sudo mv $PHANTOM_JS /usr/local/share
 sudo ln -sf /usr/local/share/$PHANTOM_JS/bin/phantomjs /usr/local/bin
 
 phantomjs --version
