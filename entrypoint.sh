7477cbb7d50fba1ef3d9d7f68995027191ac0cb1
nx-amiribenabdellah <amir.iben-abdellah@nexum.com>
2022-04-19 11:46:04 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ sudo mv $PHANTOMJS /usr/local/share
 sudo ln -sf /usr/local/share/$PHANTOMJS/bin/phantomjs /usr/local/bin
 phantomjs --version
 
-phantomjs INPUT_NAMEFILE INPUT_LINK
+phantomjs github.event.inputs.nameFile github.event.inputs.link
